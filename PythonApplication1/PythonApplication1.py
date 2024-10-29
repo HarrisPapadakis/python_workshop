8da7e7688d37046a7a75c72b4de2d8e7ea98ee49
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-10-29 21:21:13 +0200

    Add files via upload

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,3 @@
+message = "\tI told my friend, 'Python is my favorite language!'\n\tThe language 'Python' is named after Monty Python, not the snake.\n\tOne of Python's strengths is its diverse and supportive community. " 
+
+print(message)

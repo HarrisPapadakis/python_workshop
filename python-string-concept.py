a9d9cf6b7d7b5ec5b0f64f3414dc26b7ba3ce744
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-10-29 22:16:20 +0200

    Add files via upload

---
+++ python-string-concept.py
@@ -0,0 +1,14 @@
+message = "\tThis is a string\n\tThis is also a string."
+
+message1 = """\tWomen drugs and money,
+\twhat can all this tell you,
+\tAEK ole AEK ole,
+\tAEK ole AEK ole,
+\tYou are my destiny,
+\tYou take my heart and mind,
+\tYou give me a reason to exist,
+\tAnd a lifetime the same madness I will have,
+\tDouble-headed with you I get sick,
+\tAEKara I missed the train"""
+
+print(f"{message}\n{message1}")

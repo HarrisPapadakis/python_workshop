16fa0579d011432c898a5534491d2a9091426815
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-11-11 17:32:42 +0200

    Add files via upload

---
+++ My_motos_empty_list_howtoadd.py
@@ -0,0 +1,10 @@
+motorcycles = []
+
+motorcycles.append('Honda')
+motorcycles.append('Suzuki')
+motorcycles.append('Aprilia')
+motorcycles.append('Ktm')
+motorcycles.append('Vespa')
+motorcycles.append('Yamaha')
+
+print(motorcycles)
